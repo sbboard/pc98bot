3b0f093201c20253b9bf5aa6be990441325f50be
sbboard <colin.buffum@gmail.com>
2019-05-26 21:19:40 -0600

    now uses async await to prevent deletion before upload

--- app.js
+++ app.js
@@ -7,36 +7,56 @@ var fs = require('fs'),
 
 let admin = {
   time: 5*60*60*1000,
-  imgDir: '/tempimg/'
+  imgDir: '/img/'
 }
 
 var T = new Twit(config);
 
-function random_from_array(images){
-     return images[Math.floor(Math.random() * images.length)];
+//get folder
+function getFolder(){
+  return new Promise(resolve => {
+    let currentFolder = 0
+    let folders = fs.readdirSync(__dirname + admin.imgDir).filter(function (file) {
+      return fs.statSync(__dirname + admin.imgDir+'/'+file).isDirectory();
+    })
+    currentFolder = Math.floor(Math.random() * folders.length)
+    resolve(folders[currentFolder])
+  })
 }
 
-function upload_random_image(){
-  let images = []
-  let folders = []
-  let currentFolder = 0
-  var image_path = ""
-  let b64content = ""
+//get image
+function getImage(folder){
+  return new Promise(resolve => {
+    let images = []
+    let currentImg = 0
 
-  fs.readdirSync(__dirname + admin.imgDir).filter(function (file) {
-      folders.push(file)
-  })
-
-  currentFolder = Math.floor(Math.random() * folders.length)
-
-  fs.readdir(__dirname + admin.imgDir + folders[currentFolder] + "/", function(err, files){
-    files.forEach(function(f) {
-      images.push(f);
+    fs.readdirSync(__dirname + admin.imgDir + folder + "/").filter(function(file){
+        images.push(file);
     })
-    image_path = path.join(__dirname, admin.imgDir + folders[currentFolder] + "/" + random_from_array(images))
+    currentImg = Math.floor(Math.random() * images.length)
+    resolve({
+      imgName: images[currentImg],
+      imgLength: images.length
+    })
+  })
+}
+
+//tweet
+function tweet(folder, image){
+  return new Promise(resolve => {
+    let image_path = ""
+    let b64content = ""
+    let msg = ""
+    
+    image_path = path.join(__dirname, admin.imgDir + folder + "/" + image)
     b64content = fs.readFileSync(image_path, { encoding: 'base64' });
 
-
+    if(folder == "unknown"){
+      msg = "PC-98 game name unknown. If you know the name of this game, please leave a reply!"
+    }
+    else{
+      msg = `${folder} // PC-98`
+    }
     console.log('Uploading image...');
 
     T.post('media/upload', { media_data: b64content }, function (err, data, response) {
@@ -45,12 +65,12 @@ function upload_random_image(){
         console.log(err);
       }
       else{
-        console.log('Upload Image!');
+        console.log('Uploaded Image!');
         console.log('Tweeting...');
-  
+
         T.post('statuses/update', {
           media_ids: new Array(data.media_id_string),
-          status: `${folders[currentFolder]} - PC-98`
+          status: msg
         },
           function(err, data, response) {
             if (err){
@@ -60,24 +80,53 @@ function upload_random_image(){
             else{
               console.log('Posted!');
             }
+            resolve(image_path)
           }
         );
       }
     })
-    //delete image
-    // fs.unlink(image_path, function(err){
-    //      if (err){
-    //        console.log('ERROR: unable to delete image ' + image_path);
-    //      }
-    //      else{
-    //        console.log('img ' + image_path + ' was deleted');
-    //      }
-    //    });
-    //check to see if directory is empty, then delete it
   })
 }
 
-//This is the kush
-//setInterval(function(){
-  upload_random_image();
-//}, admin.time);
+//delete image
+function deleteImg(image_path){
+  return new Promise(resolve => {
+    fs.unlink(image_path, function(err){
+      if (err){
+        console.log('ERROR: unable to delete image ' + image_path);
+      }
+      else{
+        console.log('img ' + image_path + ' was deleted');
+      }
+      resolve('resolved')
+    });
+  })
+}
+
+//delete folder
+function deleteFolder(imgLength, folder){
+  return new Promise(resolve => {
+    if(imgLength - 1 < 1){
+      fs.rmdir(__dirname + admin.imgDir + folder, () => {
+        console.log(`deleted ${folder} folder`)
+        resolve('resolved')
+      })
+    }
+    else{
+      resolve('resolved')
+    }
+  })
+}
+
+async function runScript(){
+  let folderName = await getFolder()
+  let imgObj = await getImage(folderName)
+  let filepath = await tweet(folderName, imgObj.imgName)
+  await deleteImg(filepath)
+  await deleteFolder(imgObj.imgLength, folderName)
+}
+
+//Initiate
+setInterval(function(){
+  runScript()
+}, admin.time);
