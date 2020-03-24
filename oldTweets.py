00873c579e6b9ea5f7009e45ec1ed926ed6d3fb0
sbboard <colin.buffum@gmail.com>
2020-03-24 17:57:41 -0600

    now saves tweet id in json

--- oldTweets.py
+++ oldTweets.py
@@ -1,8 +1,19 @@
 import GetOldTweets3 as got
+import json
 
+data = {}
+data['id'] = []
 tweetCriteria = got.manager.TweetCriteria().setQuerySearch('from:pc98_bot #pc98')\
                                            .setSince("2019-09-01")\
                                            .setUntil("2019-09-30")\
                                            .setMaxTweets(1)
 tweet = got.manager.TweetManager.getTweets(tweetCriteria)[0]
-print(tweet.text)
+data['id'].append(tweet.id)
+with open('retweet.json', 'w') as outfile:
+    json.dump(data, outfile)
+
+#concept: script is run every time, chooses a random date between today and first post
+#checks if it's been retweeted by PC-98 bot before
+#returns an id as a json document
+
+#retweet() then reads json document for id and retweets that id
