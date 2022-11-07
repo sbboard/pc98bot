8a4d7fdca43911548b8826e981e60f32bd71b796
sbboard <colin.buffum@gmail.com>
2022-11-06 20:23:48 -0700

    cleaned up old code

--- app.js
+++ app.js
@@ -1,158 +1,88 @@
-var fs = require("fs"),
-  path = require("path"),
+const fs = require("fs"),
   Twit = require("twit"),
-  config = require(path.join(__dirname, "config.js"));
-var T = new Twit(config);
-const axios = require("axios");
-var archive = require("./archive/tweet.js");
-
-Date.prototype.addMonths = function (months) {
-  var date = new Date(this.valueOf());
-  date.setMonth(date.getMonth() + months);
-  return date;
-};
-
-Date.prototype.addDays = function (days) {
-  var date = new Date(this.valueOf());
-  date.setDate(date.getDate() + days);
-  return date;
-};
-
-//4 for normal, 1 for hourly
-const hour = 4;
-const minute = 0;
+  path = require("path"),
+  config = require(path.join(__dirname, "config.js")),
+  T = new Twit(config),
+  archive = require("./archive/tweet.js"),
+  hour = 4, //4 for normal, 1 for hourly
+  retweetEvery = 5, //0 for every time, 5 for normal
+  admin = {
+    imgDir: "/img/",
+    debug: true,
+  };
 let counter = 1;
-
-//0 for every time, 5 for normal
-const retweetEvery = 5;
-
-let admin = {
-  imgDir: "/img/",
-  hashtagNotifier: "###",
-  debug: true,
-};
 
 //get how many images are left
 async function getCount() {
-  let promise = new Promise((resolve) => {
-    fs.readdir(__dirname + admin.imgDir, (err, files) => {
-      let folderAmt = files.length;
-      resolve(folderAmt);
-    });
+  const promise = new Promise((resolve) => {
+    fs.readdir(__dirname + admin.imgDir, (err, files) => resolve(files.length));
   });
-  let result = await promise;
-  return result;
+  return await promise;
 }
 
-//get folder
 function getFolder() {
   return new Promise((resolve) => {
-    let currentFolder = 0;
-    let folders = fs
-      .readdirSync(__dirname + admin.imgDir)
-      .filter(function (file) {
-        return fs.statSync(__dirname + admin.imgDir + "/" + file).isDirectory();
-      });
-    currentFolder = Math.floor(Math.random() * folders.length);
-    resolve(folders[currentFolder]);
+    const folders = fs.readdirSync(__dirname + admin.imgDir).filter((f) => {
+      return fs.statSync(__dirname + admin.imgDir + "/" + f).isDirectory();
+    });
+    resolve(folders[Math.floor(Math.random() * folders.length)]);
   });
 }
 
-//get image
 function getImage(folder) {
   return new Promise((resolve) => {
     let images = [];
-    let currentImg = 0;
-
-    fs.readdirSync(__dirname + admin.imgDir + folder + "/").filter(function (
-      file
-    ) {
-      images.push(file);
-    });
-    currentImg = Math.floor(Math.random() * images.length);
+    fs.readdirSync(__dirname + admin.imgDir + folder + "/").forEach((file) =>
+      images.push(file)
+    );
     resolve({
-      imgName: images[currentImg],
+      imgName: images[Math.floor(Math.random() * images.length)],
       imgLength: images.length,
     });
   });
 }
 
-//tweet
 function tweet(folder, image) {
   return new Promise((resolve) => {
-    let image_path = "";
-    let b64content = "";
+    let image_path = path.join(__dirname, admin.imgDir + folder + "/" + image);
+    const b64content = fs.readFileSync(image_path, { encoding: "base64" });
     let msg = "";
-
-    image_path = path.join(__dirname, admin.imgDir + folder + "/" + image);
-    b64content = fs.readFileSync(image_path, { encoding: "base64" });
-
     if (folder == "_unknown") {
       msg = `unknown // PC-98
 If you know the name of this software, please leave a reply!
 あなたがこのソフトウェアの名前を知っているならば、答えを残してください`;
     } else {
-      if (folder.includes(admin.hashtagNotifier)) {
-        let gameName = folder.split("###")[0];
-        let company = folder.split("###")[1];
-        msg = `${gameName} // ${company} // PC-98 // #pc98 #${company.replace(
-          /[!@#$'%^()\-&*\[\]\s,.]/g,
-          ""
-        )}`;
-      } else {
-        msg = `${folder} // PC-98 // #pc98`;
-      }
+      const gameName = folder.split("###")[0],
+        company = folder.split("###")[1],
+        company_hash = company.replace(/[!@#$'%^()\-&*\[\]\s,.]/g, "");
+      msg = `${gameName} // ${company} // PC-98 // #pc98 #${company_hash}`;
     }
     console.log("Uploading image...");
-
-    T.post(
-      "media/upload",
-      { media_data: b64content },
-      function (err, data, response) {
-        if (err) {
-          console.log("ERROR:");
-          console.log(err);
-        } else {
-          console.log("Uploaded Image!");
-          console.log("Tweeting...");
-
-          T.post(
-            "statuses/update",
-            {
-              media_ids: new Array(data.media_id_string),
-              status: msg,
-            },
-            function (err, data, response) {
-              if (err) {
-                console.log("ERROR:");
-                console.log(err);
-              } else {
-                console.log("Posted!");
-              }
-              resolve(image_path);
-            }
-          );
-        }
+    T.post("media/upload", { media_data: b64content }, (err, data) => {
+      if (err) console.log("ERROR:", err);
+      else {
+        console.log("Uploaded Image!");
+        const mid = new Array(data.media_id_string);
+        T.post("statuses/update", { media_ids: mid, status: msg }, (err) => {
+          if (err) console.log("ERROR:", err);
+          else console.log("Posted!");
+          resolve(image_path);
+        });
       }
-    );
+    });
   });
 }
 
-//delete image
 function deleteImg(image_path) {
   return new Promise((resolve) => {
-    fs.unlink(image_path, function (err) {
-      if (err) {
-        console.log("ERROR: unable to delete image " + image_path);
-      } else {
-        console.log("img " + image_path + " was deleted");
-      }
+    fs.unlink(image_path, (err) => {
+      if (err) console.log("ERROR: unable to delete image " + image_path);
+      else console.log("img " + image_path + " was deleted");
       resolve("resolved");
     });
   });
 }
 
-//delete folder
 function deleteFolder(imgLength, folder) {
   return new Promise((resolve) => {
     if (imgLength - 1 < 1) {
@@ -160,75 +90,42 @@ function deleteFolder(imgLength, folder) {
         console.log(`deleted ${folder} folder`);
         resolve("resolved");
       });
-    } else {
-      resolve("resolved");
-    }
+    } else resolve("resolved");
   });
 }
+
 function postRT(RTID) {
-  T.post(
-    "statuses/retweet/:id",
-    {
-      id: RTID,
-    },
-    function (err, response) {
-      if (response) {
-        console.log("retweeted");
-      }
-      if (err) {
-        if (err.code == 327) {
-          //untweet
-          T.post(
-            "statuses/unretweet/:id",
-            {
-              id: RTID,
-            },
-            function (err, response) {
-              if (response) {
-                console.log("unretweeted");
-                postRT(RTID);
-              }
-              if (err) {
-                console.log(err);
-              }
-            }
-          );
-          //
-        } else if (err.code == 144) {
-          retweet();
-        } else {
-          console.log(err);
-        }
-      }
+  T.post("statuses/retweet/:id", { id: RTID }, (err, response) => {
+    if (response) console.log("retweeted");
+    if (err) {
+      if (err.code == 327) {
+        //untweet
+        T.post("statuses/unretweet/:id", { id: RTID }, (err, response) => {
+          if (response) postRT(RTID);
+          if (err) console.log(err);
+        });
+      } else if (err.code == 144) retweet();
+      else console.log(err);
     }
-  );
+  });
 }
 
 async function retweet() {
   const archiveLength = archive.length;
-  let randomNo = Math.floor(Math.random() * archiveLength);
-  if (
-    archive[randomNo].tweet.entities.hashtags.some(
-      (item) => item.text === "pc98"
-    )
-  ) {
-    postRT(archive[randomNo].tweet.id);
-  } else {
-    retweet();
-  }
+  const rand = Math.floor(Math.random() * archiveLength);
+  if (archive[rand].tweet.entities.hashtags.some((i) => i.text === "pc98")) {
+    postRT(archive[rand].tweet.id);
+  } else retweet();
 }
 
 async function runScript() {
-  var time = new Date();
-  var h = time.getHours();
-  var m = time.getMinutes();
-  if ((h % hour == 0 && parseInt(m) == minute) || admin.debug == true) {
-    let theCount = getCount();
-    theCount.then(async function (numOfGames) {
+  const time = new Date();
+  if ((time.getHours() % hour == 0 && time.getMinutes() == 0) || admin.debug) {
+    getCount().then(async function (numOfGames) {
       if (numOfGames > 0 && counter < retweetEvery) {
-        let folderName = await getFolder();
-        let imgObj = await getImage(folderName);
-        let filepath = await tweet(folderName, imgObj.imgName);
+        const folderName = await getFolder();
+        const imgObj = await getImage(folderName);
+        const filepath = await tweet(folderName, imgObj.imgName);
         await deleteImg(filepath);
         await deleteFolder(imgObj.imgLength, folderName);
         counter++;
@@ -240,14 +137,6 @@ async function runScript() {
   }
 }
 
-if (admin.debug == true) {
-  setInterval(function () {
-    runScript();
-  }, 10000);
-  console.log("admin mode on");
-} else {
-  setInterval(function () {
-    runScript();
-  }, 60000);
-  console.log("normal mode on");
-}
+let timeInterval = admin.debug ? 10000 : 60000;
+setInterval(runScript, timeInterval);
+console.log("admin mode:", admin.debug);
